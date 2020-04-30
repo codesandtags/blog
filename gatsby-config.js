e0147ec74e242572d0f5975fcd835d5a68ff3a1d
Edwin Torres <codesandtags@gmail.com>
2020-04-30 13:21:34 -0500

    chore: Added configuration to publish content

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  pathPrefix: "/codesandtags.github.io",
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Codes and Tags`,
+    author: `Edwin Torres`,
+    description: `My site description...`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+}
