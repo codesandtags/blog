c55cc5146f1ec834f4b459a39a8aeec56e0124e0
Edwin Torres <codesandtags@gmail.com>
2020-04-30 23:03:34 -0500

    feature: Added Mailchimp integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         contentAuthors: "content/authors",
         basePath: "/",
         authorsPage: true,
-        mailchimp: false, // make sure this is true!
+        mailchimp: true, // make sure this is true!
         sources: {
           local: true,
           // contentful: true,
@@ -61,6 +61,13 @@ module.exports = {
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
         trackingId: "UA-25918076-1",
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        // add your MC list endpoint here; see plugin repo for instructions
+        endpoint: 'https://github.us8.list-manage.com/subscribe/post?u=8dbe735d8d5499c1098e8f8ff&amp;id=6324a629f0',
       },
     },
   ],
