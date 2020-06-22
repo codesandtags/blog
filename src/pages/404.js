b997fe603c55fa33e908b873d4207fcee8413776
Edwin Torres <codesandtags@gmail.com>
2020-06-21 21:09:15 -0500

    feature: Updated novela theme dependenvy version and added 404 images

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,17 +5,22 @@ import Section from "@narative/gatsby-theme-novela/src/components/Section";
 import SEO from "@narative/gatsby-theme-novela/src/components/SEO";
 import Headings from "@narative/gatsby-theme-novela/src/components/Headings";
 
+import notFoundPageImage from './404-error.gif';
+
 function NotFoundPage() {
-  return (
-    <Layout>
-      <SEO />
-      <Section>
-        <div style={{ marginTop: "100px" }}>
-          <Headings.h1>404: Page Not Found</Headings.h1>
-        </div>
-      </Section>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <SEO/>
+            <Section>
+                <div style={{ marginTop: "100px" }}>
+                    <Headings.h1>404: Page Not Found</Headings.h1>
+                </div>
+                <div>
+                    <img src={notFoundPageImage} width="100%"/>
+                </div>
+            </Section>
+        </Layout>
+    );
 }
 
 export default NotFoundPage;
